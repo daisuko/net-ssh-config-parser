9842a3340854b528b7566e644a49f807a42d416a
daisuko <striker.daisuko@gmail.com>
2016-12-30 10:45:33 +0900

    implement Net::SSH::Config::Parser

--- lib/net/ssh/config/parser.rb
+++ lib/net/ssh/config/parser.rb
@@ -4,7 +4,49 @@ module Net
   module SSH
     module Config
       module Parser
-        # Your code goes here...
+        class << self
+          def load(filename)
+            return nil unless File.exist?(filename)
+
+            parse(open(filename, &:read))
+          end
+
+          def parse(value)
+            inner_parse(value.lines.map(&:chomp).reject { |line| line.empty? })
+          end
+
+          private
+
+          def inner_parse(lines, hosts = {})
+            first, rest = lines[0], lines[1..-1]
+            key, *names = first.split(' ')
+            options, rest = option_parse(rest)
+            ret = hosts.merge(names.each_with_object({}) { |e, a| a[e] = options })
+
+            rest.empty? ? ret : inner_parse(rest, ret)
+          end
+
+          def option_parse(lines, options = {})
+            first, rest = lines[0], lines[1..-1]
+
+            if rest && first.start_with?(' ')
+              key, value = first.split(' ', 2)
+
+              option_parse(rest, options.merge(key => cast(value)))
+            else
+              [options, lines]
+            end
+          end
+
+          def cast(value)
+            case value
+            when /^\d+$/  then value.to_i
+            when /^yes$/i then true
+            when /^no$/i  then false
+            else value
+            end
+          end
+        end
       end
     end
   end
