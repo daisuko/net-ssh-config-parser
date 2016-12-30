3e8dc6d4ab19ac6e8962a395fc6382497553b23d
daisuko <striker.daisuko@gmail.com>
2016-12-30 09:26:05 +0900

    initial commit

---
+++ lib/net/ssh/config/parser.rb
@@ -0,0 +1,11 @@
+require "net/ssh/config/parser/version"
+
+module Net
+  module Ssh
+    module Config
+      module Parser
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/net/ssh/config/parser/version.rb
@@ -0,0 +1,9 @@
+module Net
+  module Ssh
+    module Config
+      module Parser
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
